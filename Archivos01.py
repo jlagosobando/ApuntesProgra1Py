31abf57b1b14af03fb6acb920fbc9dc453712e41
Juan Lagos <jlagosobando@gmail.com>
2019-06-05 12:21:04 -0400

    Create Archivos01.py

---
+++ Archivos01.py
@@ -0,0 +1,47 @@
+#abrir archivo para para escritura, si archivo no existe se crea
+f=open("archivo.txt","w")
+f.write("test")
+f.close()
+
+#abrir archivo para escritura,  guarda textos con salto de linea
+f=open("archivo.txt","w")
+for i in range(60, 94):
+    f.write("ascii "+str(i)+" "+chr(i)+"\n")
+f.close()
+
+#abrir archivo para escritura,  guarda enteros
+f=open("archivo.txt","w")
+for i in range(21):
+    f.write(str(i))
+f.close()
+
+#abrir archivo para escritura,  guarda enteros con salto de linea
+f=open("archivo.txt","w")
+for i in range(21):
+    f.write(str(i)+"\n")
+f.close()
+
+#abrir archivo para lectura, lee solo una linea
+f=open("archivo.txt","r")
+texto=f.readline()
+print(texto)
+
+#abrir archivo para lectura, lee linea a linea e imprime
+f=open("archivo.txt","r")
+while True:
+    texto=f.readline()
+    if texto=="":
+        break
+    print(texto)
+
+#abrir archivo para lectura, lee linea a linea e imprime
+f=open("archivo.txt","r")
+for line in f:
+    print(line)
+
+#abrir archivo para lectura, lo recorre linea a linea, aplica strip, transforma a entero e imprime
+f=open("archivo.txt","r")
+for line in f:
+    line=line.strip()
+    number=int(line)
+    print (number)
