5572fa5e3870fcc7940edf984ab3866b90d27e42
Juan Lagos <jlagosobando@gmail.com>
2020-03-04 16:30:09 -0300

    Update ActRegNombres.py

--- ActRegNombres.py
+++ ActRegNombres.py
@@ -12,10 +12,31 @@ file.close()
 #3 pida al usuario los apellidos para cada estudiante por teclado
 #modifique al arreglo agregando esta info, sin aumentar dimensionalidad ni número de casillas
 
+for i in range(len(Est)):
+  apellido=input("Escriba apellido "+str(i)+": ")
+  Est[i]=Est[i]+" "+apellido
+
+print(Est)
+
 
 #cree un nuevo arreglo, agregando una nueva dimensión
 #para cada estudiante en Est, deberá asociar un registro academico ingresado por teclado
 
+Registro=[]
+
+for e in Est:
+  reg=input("ingrese n° de registro de "+str(e)+" ")
+  temp=[e,reg]
+  Registro.append(temp)
+
+print(Registro)
 
 #guarde en un archivo reg.txt el resultado de su arreglo, un estudiante por linea
 #separando nombre y registro por un espacio
+
+file=open("reg.txt","w")
+
+for e in Registro:
+  file.write(e[0]+" "+str(e[1])+"\n")
+
+file.close()
